fed23ea8a055ad5cfbcb187af625ed5864d97cc4
syed huzaifa ali <huzaifaali03341@gmail.com>
2023-06-25 00:50:36 +0500

    17

---
+++ bin/17.dart
@@ -0,0 +1,9 @@
+void main() {
+  List<int> originalList = [1, 2, 3, 4, 5];
+  List<int> squaredList = squareList(originalList);
+  print(squaredList);
+}
+
+List<int> squareList(List<int> list) {
+  return list.map((int value) => value * value).toList();
+}
